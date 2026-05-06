eada497a09e0726be49d8102586c0276b78be574
pauldavid23 <paul.david.gelario@gmail.com>
2026-05-06 22:26:15 +0800

    Adding these changes

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> j = new ArrayDeque<>();
+        j.add(1);
+        j.add(2);
+
+        System.out.println(j);
+
+    }
+}
