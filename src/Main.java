13829f49330aab258685319552d00e8c120a1c45
pauldavid23 <paul.david.gelario@gmail.com>
2026-05-06 22:33:10 +0800

    adding more changes

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ArrayDeque<Integer> j = new ArrayDeque<>();
+
         j.add(1);
-        j.add(2);
+        j.offerLast(3);
+        j.offerLast(2);
 
         System.out.println(j);
 
