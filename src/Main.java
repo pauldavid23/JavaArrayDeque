e30ff1b5e2dae584d1ead16cc417238929d64b73
pauldavid23 <paul.david.gelario@gmail.com>
2026-05-11 20:27:01 +0800

    add

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         ArrayDeque<Integer> j = new ArrayDeque<>();
 
         for (int i = 10; i<=10;i++) {
-            j.add(i);
+            j.addLast(i);
             System.out.println(i);
         }
 
